6298b56039aff2940bdf574e482e5da4ed432659
Jeff Chiu <jwchiu82@gmail.com>
2020-11-02 13:06:34 -0800

    add more js BL for page
    Co-authored-by: Danielle Thompson <71739178+dani-t-codes@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,34 @@
-$(document).ready(function) {
-
+//business logic for destinations
+function Destinations() {
+  this.places = []; //e.g. Canada, USA, Japan etc. 
+  this.placeID = 0;
 }
+
+Destinations.prototype.addPlace = function(place) {
+  place.id = this.assignId();
+  this.places.push(place);
+}
+
+Destinations.prototype.assignId = function() {
+  this.placeID += 1;
+  return this.placeID;
+}
+
+//business logic for location details
+function Location(location, landmark, when, notes, recommendations, restaurants) { //add country?
+  this.location = location;
+  this.landmark = landmark;
+  this.when = when;
+  this.notes = notes;
+  this.recomendations = recommendations;
+  this.restaurants = restaurants;
+}
+
+$(document).ready(function) {
+  
+}
+
+
+//"Largest"/parent designation: Destination (e.g. Spain)
+//1st level child designation: Location (e.g. Barcelona)
+//2nd level child designation: landmark, when, notes, recs, restaurants
